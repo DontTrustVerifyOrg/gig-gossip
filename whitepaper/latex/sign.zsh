89a8048df9c359cd8fee87eb1ba3c3bf0b3e6920
sonofsatoshi <pawel@donttrustverify.org>
2023-02-21 11:39:15 +1100

    signatures

--- whitepaper/latex/sign.zsh
+++ whitepaper/latex/sign.zsh
@@ -1,2 +1,2 @@
 gpg --detach-sig --output=gig_gossip.sig gig_gossip.pdf
-sha512sum gig_gossip.sig | cut -d " " -f 1 >gig_gossip.sig.sha256
+sha256sum gig_gossip.sig | cut -d " " -f 1 >gig_gossip.sig.sha256
