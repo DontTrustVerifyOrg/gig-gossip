2473f373ed85abc0433e3e2e73996ab341a37d2d
sonofsatoshi <pawel@donttrustverify.org>
2023-10-19 17:38:26 +1100

    more middleware

--- net/NGigGossip4Nostr/NGigGossipApp/ViewModels/Ride/Customer/LookingDriverViewModel.cs
+++ net/NGigGossip4Nostr/NGigGossipApp/ViewModels/Ride/Customer/LookingDriverViewModel.cs
@@ -20,6 +20,8 @@ namespace GigMobile.ViewModels.Ride.Customer
 
         public override async Task Initialize()
         {
+            await base.Initialize();
+
             var fromGh = GeoHash.Encode(latitude: 42.6, longitude: -5.6, numberOfChars: 7);
             var toGh = GeoHash.Encode(latitude: 42.5, longitude: -5.6, numberOfChars: 7);
 
@@ -27,15 +29,13 @@ namespace GigMobile.ViewModels.Ride.Customer
            // trs.Last().Certificate; 
             var certificate = Crypto.DeserializeObject<Certificate>(new byte[] { });//need to load proper certificate
 
-            _gigGossipNode.BroadcastTopicAsync(new TaxiTopic()
+            await _gigGossipNode.BroadcastTopicAsync(new TaxiTopic()
             {
                 FromGeohash = fromGh,
                 ToGeohash = toGh,
                 PickupAfter = DateTime.Now,
                 DropoffBefore = DateTime.Now.AddMinutes(20)
             },certificate);
-
-            base.Initialize();
         }
     }
 }
