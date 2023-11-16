3ceb6d5d40f11827d620e9b047104f760821faa9
Pawel Kaplanski <p.kaplanski@cognitum.eu>
2023-11-16 15:16:06 +1100

    initializing ridesharecliapp

--- net/NGigGossip4Nostr/GigWorkerTest/Program.cs
+++ net/NGigGossip4Nostr/GigWorkerTest/Program.cs
@@ -6,18 +6,35 @@ using NGigTaxiLib;
 using NGeoHash;
 using NBitcoin.Protocol;
 using System.Diagnostics;
-
+using Microsoft.Extensions.Configuration;
+using CommandLine;
 
 internal class Program
 {
 
-    static void Main(string[] args)
+    public class Options
     {
-        //new GigWorkerBasicTest.BasicTest(args).RunAsync().Wait();
-        //new GigWorkerMediumTest.MediumTest(args).RunAsync().Wait();
-        new GigWorkerComplexTest.ComplexTest(args).RunAsync().Wait();
-        Console.WriteLine("END");
+        [Option('b', "basic", Required = false, HelpText = "Run basic test")]
+        public bool Basic { get; set; }
+        [Option('m', "medium", Required = false, HelpText = "Run medium test")]
+        public bool Medium { get; set; }
+        [Option('c', "complex", Required = false, HelpText = "Run complex test")]
+        public bool Complex { get; set; }
     }
 
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+                           .WithParsed<Options>(o =>
+                           {
+                               if (o.Basic)
+                                   new GigWorkerBasicTest.BasicTest(args).RunAsync().Wait();
+                               if (o.Medium)
+                                   new GigWorkerMediumTest.MediumTest(args).RunAsync().Wait();
+                               if (o.Complex)
+                                   new GigWorkerComplexTest.ComplexTest(args).RunAsync().Wait();
+                           });
+        Console.WriteLine("END");
+    }
 }
 
---
+++ net/NGigGossip4Nostr/RideShareCLIApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RideShareCLIApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
