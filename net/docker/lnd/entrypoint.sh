effa5ea15050a501b5ad746b0a1c243875647b6d
art <wazartur@gmail.com>
2024-10-30 11:49:44 +0100

    allow to start lnd in init mode

--- net/docker/lnd/entrypoint.sh
+++ net/docker/lnd/entrypoint.sh
@@ -44,41 +44,48 @@ else
 fi
 
 
-
-if [ -n "$AZURE_KEY_VAULT_URL" ] && [ -n "$AZURE_CLIENT_ID" ] && [ -n "$AZURE_CLIENT_SECRET" ] && [ -n "$AZURE_TENANT_ID" ]; then
-    echo "Fetching access token from Azure Key Vault"
-    ACCESS_TOKEN=$(curl -sS --fail -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$AZURE_CLIENT_ID&client_secret=$AZURE_CLIENT_SECRET&resource=https://vault.azure.net" https://login.microsoftonline.com/$AZURE_TENANT_ID/oauth2/token | jq -r .access_token)
-    if [ $? -ne 0 ] || [ -z "$ACCESS_TOKEN" ]; then
-        echo "Error: Failed to retrieve access token from Azure Key Vault"
+if [ -n "$INIT" ]; then
+    if [ -n "$AZURE_KEY_VAULT_URL" ] && [ -n "$AZURE_CLIENT_ID" ] && [ -n "$AZURE_CLIENT_SECRET" ] && [ -n "$AZURE_TENANT_ID" ]; then
+        echo "Fetching access token from Azure Key Vault"
+        ACCESS_TOKEN=$(curl -sS --fail -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$AZURE_CLIENT_ID&client_secret=$AZURE_CLIENT_SECRET&resource=https://vault.azure.net" https://login.microsoftonline.com/$AZURE_TENANT_ID/oauth2/token | jq -r .access_token)
+        if [ $? -ne 0 ] || [ -z "$ACCESS_TOKEN" ]; then
+            echo "Error: Failed to retrieve access token from Azure Key Vault"
+            exit 1
+        fi
+        echo "Access token fetched successfully from Azure Key Vault"
+        
+        echo "Fetching secret key from Azure Key Vault"
+        SECRET_KEY=$(curl -sS --fail -H "Authorization: Bearer $ACCESS_TOKEN" https://$AZURE_KEY_VAULT_URL.vault.azure.net/secrets/secret-key?api-version=7.0 | jq -r .value)
+        if [ $? -ne 0 ] || [ -z "$SECRET_KEY" ]; then
+            echo "Error: Failed to fetch secret key from Azure Key Vault"
+            exit 1
+        fi
+        echo "$SECRET_KEY" > /secret/password.txt
+        echo "Secret key saved to /secret/password.txt"
+    elif [ -n "$GITHUB_PASSWORD_URL" ] && [ -n "$GITHUB_USERNAME" ] && [ -n "$GITHUB_TOKEN" ]; then
+        echo "Fetching secret key from GitHub"
+        curl -sS --fail -u $GITHUB_USERNAME:$GITHUB_TOKEN -o /secret/password.txt $GITHUB_PASSWORD_URL
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to fetch secret key from GitHub"
+            exit 1
+        fi
+        echo "Secret key saved to /secret/password.txt"
+    elif [ -e /secret/password.txt ]; then
+        echo "Using existing secret key file in /secret/password.txt"
+    else
+        echo "Error: Secret key not provided"
         exit 1
     fi
-    echo "Access token fetched successfully from Azure Key Vault"
-    
-    echo "Fetching secret key from Azure Key Vault"
-    SECRET_KEY=$(curl -sS --fail -H "Authorization: Bearer $ACCESS_TOKEN" https://$AZURE_KEY_VAULT_URL.vault.azure.net/secrets/secret-key?api-version=7.0 | jq -r .value)
-    if [ $? -ne 0 ] || [ -z "$SECRET_KEY" ]; then
-        echo "Error: Failed to fetch secret key from Azure Key Vault"
-        exit 1
-    fi
-    echo "$SECRET_KEY" > /secret/password.txt
-    echo "Secret key saved to /secret/password.txt"
-elif [ -n "$GITHUB_PASSWORD_URL" ] && [ -n "$GITHUB_USERNAME" ] && [ -n "$GITHUB_TOKEN" ]; then
-    echo "Fetching secret key from GitHub"
-    curl -sS --fail -u $GITHUB_USERNAME:$GITHUB_TOKEN -o /secret/password.txt $GITHUB_PASSWORD_URL
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to fetch secret key from GitHub"
-        exit 1
-    fi
-    echo "Secret key saved to /secret/password.txt"
-elif [ -e /secret/password.txt ]; then
-    echo "Using existing secret key file in /secret/password.txt"
-else
-    echo "Error: Secret key not provided"
-    exit 1
 fi
 
 
 echo
-echo "Starting: lnd --lnddir=/app_data --wallet-unlock-password-file=/secret/password.txt"
-echo
-lnd --lnddir=/app_data --wallet-unlock-password-file=/secret/password.txt
+if [ -n "$INIT" ]; then
+    echo "Starting in init mode: lnd --lnddir=/app_data"
+    echo
+    lnd --lnddir=/app_data
+else
+    echo "Starting: lnd --lnddir=/app_data --wallet-unlock-password-file=/secret/password.txt"
+    echo
+    lnd --lnddir=/app_data --wallet-unlock-password-file=/secret/password.txt
+fi
